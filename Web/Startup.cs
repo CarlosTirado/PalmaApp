63f6f308454bce3190c9db6f15c56394575eceba
CarlosTirado <orley333@hotmail.com>
2020-10-03 11:24:25 -0500

    se quita prueba de crear anotacion para webapi

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -33,12 +33,6 @@ namespace TestCore5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddMvcOptions(options =>
-                {
-                    options.Filters.Add(new ValidateModelAttribute());
-                });
-
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("PalmAppContext")));
