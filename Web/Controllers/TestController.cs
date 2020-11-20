7f66fc52dc78138af40ea5328605c400dcac903c
Samir Miranda <Bya_smiranda@outlook.com>
2020-11-19 23:36:31 -0500

    se confirma mejoras poderosas en la implementacion del singleton  para obtener la informacion de la finca

---
+++ Web/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.DatosBasicos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datosFinca = DatosFinca.GetDatosFinca();
+            return Ok(datosFinca);
+        }
+    }
+}
