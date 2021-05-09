7236ec20cbe5ecdbc9a05bb22bc0b475a4dcfec9
CarlosTirado <orley333@hotmail.com>
2021-05-08 20:09:45 -0500

    ...

---
+++ Web/Controllers/TestS3Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class TestS3 : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
