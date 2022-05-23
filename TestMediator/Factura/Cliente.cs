18c0a27f2115aa0b40c82e20b95fcdb1ddfd1603
CarlosTirado <orley333@gmail.com>
2022-05-23 11:49:32 -0500

    Prueba modelo para clase

---
+++ TestMediator/Factura/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMediator.Factura
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+
+    public class ClienteAgenteRetenedor: Cliente
+    {
+        public decimal PorcentajeDeRetencion { get; set; }
+    }
+
+    public class ClienteResponsableDeIva : Cliente
+    {
+    }
+}
---
+++ TestMediator/Factura/TipoFactura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMediator.Factura
+{
+    public class TipoFactura
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
