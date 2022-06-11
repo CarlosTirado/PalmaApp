f6fca037385b922a6ea0af63bae14ae62ba72b5a
CarlosTirado <orley333@gmail.com>
2022-06-11 13:58:24 -0500

    Implementacion de patron singleton

---
+++ TestMediator/Singleton/RecursosApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMediator.Singleton
+{
+    public class RecursosApp
+    {
+        public string LogoEntidad { get; private set; }
+        public string NombreEntidad { get; private set; }
+
+        private static RecursosApp instance;
+
+        private RecursosApp()
+        {
+            ConsultarRecursosApp();
+        }
+
+        public static RecursosApp GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new RecursosApp();
+            }
+
+            return instance;
+        }
+
+
+        private void ConsultarRecursosApp()
+        {
+            //Consulta a BD
+            LogoEntidad = "Base64 con logo de la entidad";
+            NombreEntidad = "Nombre de la entidad";
+        }
+    }
+}
---
+++ TestMediator/Singleton/TestSingleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMediator.Singleton
+{
+    public class TestSingleton
+    {
+        public void Main()
+        {
+            RecursosApp recursosApp = RecursosApp.GetInstance();
+            RecursosApp recursosApp3 = RecursosApp.GetInstance();
+            RecursosApp recursosApp4 = RecursosApp.GetInstance();
+        }
+    }
+}
